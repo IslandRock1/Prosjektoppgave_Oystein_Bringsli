d8d17a3ceac544358e7fefa81c56d451d4781be8
Øystein Bringsli <oysteinmb@gmail.com>
2023-10-03 19:55:41 +0200

    init

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,13 @@
 #include "threepp/extras/imgui/ImguiContext.hpp"
 #include "threepp/threepp.hpp"
 
+#include <iostream>
+
 using namespace threepp;
-
-namespace {
-
-    auto createBox(const Vector3& pos, const Color& color) {
-        auto geometry = BoxGeometry::create();
-        auto material = MeshBasicMaterial::create();
-        material->color.copy(color);
-        auto mesh = Mesh::create(geometry, material);
-        mesh->position.copy(pos);
-
-        return mesh;
-    }
-
-}// namespace
 
 int main() {
 
-    Canvas canvas("threepp demo", {{"aa", 4}});
+    Canvas canvas("Particle Simulator", {{"aa", 4}});
     GLRenderer renderer(canvas.size());
     renderer.setClearColor(Color::aliceblue);
 
@@ -30,11 +18,6 @@ int main() {
     OrbitControls controls{*camera, canvas};
 
     auto scene = Scene::create();
-
-    auto group = Group::create();
-    group->add(createBox({-1, 0, 0}, Color::green));
-    group->add(createBox({1, 0, 0}, Color::blue));
-    scene->add(group);
 
     renderer.enableTextRendering();
     int textYOffset = 30;
@@ -61,13 +44,13 @@ int main() {
 
     Clock clock;
     float rotationSpeed = 1;
+    int name = 1;
+
     canvas.animate([&] {
         auto dt = clock.getDelta();
-        group->rotation.y += rotationSpeed * dt;
 
         renderer.render(*scene, *camera);
 
-        ui.render();
-        group->position.fromArray(posBuf);
+        auto& textHandle = renderer.textHandle();
     });
 }
