0e7ca5d953bf4c3c087913c7bd58431b335667bf
Øystein Bringsli <oysteinmb@gmail.com>
2023-11-02 18:29:31 +0100

    Added new example

---
+++ examples/SimpleExample.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Øystein Bringsli on 2/11/2023.
+//
+
+#include "ThreeppHandler.hpp"
+#include "ParticleHandler.hpp"
+#include "GravityControl.hpp"
+#include "Vec3.hpp"
+
+int main() {
+
+    Vec3 boundingBoxSize = {100, 100, 100};
+    ParticleHandler particleHandler = ParticleHandler(boundingBoxSize, 5, 10);
+    particleHandler.setGravityType(GravityType::NoGravity);
+    particleHandler.setMinSpeed(0.1);
+    particleHandler.setFriction(0.99);
+
+    ThreeppHandler handler{"Particle Simulator", particleHandler};
+    handler.setRandomColor(true);
+
+    handler.addText("Frame 0", 0, 0, 1.5);
+    handler.addText("Particles: 0", 0, 20, 1.5);
+
+    handler.setWindowResizeListener();
+    handler.CanvasAnimate();
+}
