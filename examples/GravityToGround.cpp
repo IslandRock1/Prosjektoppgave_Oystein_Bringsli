1ca9299cb3f2410c250cf31e889c4896bff85cf0
Øystein Bringsli <oysteinmb@gmail.com>
2023-11-24 20:15:33 +0100

    Refactor

--- examples/GravityToGround.cpp
+++ examples/GravityToGround.cpp
@@ -10,17 +10,18 @@
 int main() {
 
     Vec3 boundingBoxSize = {100, 100, 100};
-    ParticleHandler particleHandler = ParticleHandler(boundingBoxSize, 1000, 2);
+    ParticleHandler particleHandler = ParticleHandler(boundingBoxSize, 300, 2);
     particleHandler.setGravityType(GravityType::Ground);
-    particleHandler.setTimeBetweenParticles(20);
+    particleHandler.setTimeBetweenParticles(3);
     particleHandler.setMinSpeed(0.1);
-    particleHandler.setFriction(0.999);
+    particleHandler.setFriction(0.99);
 
     ThreeppHandler handler{"Particle Simulator", particleHandler};
     handler.setColor(1, 1, 1);
 
     handler.particleText = true;
     handler.fpsText = true;
+    handler.maxParticleText = true;
 
     MyKeyListener myKeyListener(particleHandler);
     handler.addKeyListener(myKeyListener);
